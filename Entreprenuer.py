9477743bfe0033154e01aaff6fc169583d08e4d1
Gourav Ahlawat <cse210001019@gmail.com>
2022-08-28 11:09:44 +0530

    requirements

--- Entreprenuer.py
+++ Entreprenuer.py
@@ -7,6 +7,7 @@ from google.oauth2 import service_account
 from streamlit_elements import elements
 from streamlit_elements import html
 from streamlit_elements import mui
+import tomli
 
 
 
@@ -71,7 +72,7 @@ if choice=='Login':
         if bio == 'Home':
           @st.experimental_singleton
           def get_db():
-            key_dict = json.loads(st.secrets["textkey"])
+            key_dict = tomli.load(st.secrets1["textkey"])
             creds = service_account.Credentials.from_service_account_info(key_dict)
             db = firestore.Client(credentials=creds, project="hackathon-57ace")
             return db
